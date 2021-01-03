d6add4c392ae5aa47d05c36b57a4657f278c9124
Wood Factory J <woodfactoryj@gmail.com>
2021-01-03 23:51:23 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# lihkg-user-friendly-gallery
+
+Remember and auto-scroll to the last viewed image position in gallery mode
---
+++ index.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         LIHKG User Friendly Gallery Mode
+// @namespace
+// @version      0.1.0
+// @description  Remember and auto-scroll to the last viewed image position in gallery mode
+// @author       木廠仔, inspired by python3 巴打
+// @match        *://lihkg.com/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  // Your code here...
+
+})();
